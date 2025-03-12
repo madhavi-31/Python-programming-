9a21136bdd540b4ca3e5025b9ddbe58cfc4f858c
madhavi-31 <madhaviloya31@gmail.com>
2025-03-12 20:08:19 +0530

    Create Denomination.py

---
+++ Denomination.py
@@ -0,0 +1,13 @@
+amount=650
+five_hundred_notes=amount//500
+remaing= amount%500
+hundred= remaing//100
+remaing_1=remaing%100
+fifty= remaing_1//50
+remaing_2=remaing_1%50
+ten= remaing_2//10
+remaing_3=remaing_2%10
+print('Five hundred Notes(500):',five_hundred_notes)
+print('Hundred Notes(100):',hundred)
+print('Fifty Notes(50):',fifty)
+print('Ten notes(10):',ten)
