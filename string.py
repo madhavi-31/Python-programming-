4640a2df4047f8f7500d3c28d1f808b324335af9
madhavi-31 <madhaviloya31@gmail.com>
2025-03-12 20:00:56 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,8 @@
+#string
+astr="1a0"
+if astr[1]=="a":
+  print(int(astr[0])&int(astr[2]))
+elif astr[1]=="b":
+  print(int(astr[0])|int(astr[2]))
+elif astr[1]=="c":
+ print(int(astr[0])^int(astr[2]))
