c2a43bbee25ad3d6181ebd80d71cab5b9ab421d3
madhavi-31 <madhaviloya31@gmail.com>
2025-03-12 20:04:02 +0530

    Create season classification.py

---
+++ season classification.py
@@ -0,0 +1,12 @@
+# Season classification
+month=int(input())
+if month==4 or month==5 or month==6:
+  print("summer")
+elif month== 7 or month==8 or month==9:
+  print("Rainy")
+elif month== 10 or month==11 or month==12 or month==1:
+  print("winter")
+elif month==2 or month==3:
+  print("autumn")
+else:
+  print(" Invalid month")
